cf4e31da9c63df81eb142f5607725aa53efaa393
Samuel Grant <samuelgrant@eduroam-int-dhcp-97-154-174.ucl.ac.uk>
2018-12-06 21:40:27 +0000

    Working FFT code

--- FFT.cc
+++ FFT.cc
@@ -6,11 +6,12 @@
 #include <TH1D.h>
 #include <TMath.h>
 #include <TCanvas.h>
-
+#include <TH1F.h>
+//#include <TPad.h>
 using namespace std;
 
 int main() {
-
+  /*
   TH1F* EuropaFitter::RescaleAxis(TH1* input, Double_t Scale) {
      int bins = input->GetNbinsX();
      TAxis* xaxis = input->GetXaxis();
@@ -27,14 +28,14 @@ int main() {
      }
      return out;
 }
-  
+  */
 //A function to sample
   double start = 0;
   double end = 4*TMath::Pi();
   TF1 *fsin = new TF1("fsin", "sin(x)", start, end);//4*TMath::Pi());
    fsin->Draw();
    
-    Int_t n=25;
+    Int_t n=100;
     TH1D *hsin = new TH1D("hsin", "hsin", n+1, start, end);//0, 4*TMath::Pi());
     Double_t x;
     
@@ -57,13 +58,44 @@ int main() {
     //    hm = hsin->FFT(hm, "MAG");
     //Rescale x axis
     hm = hsin->FFT(hm,"MAG");
-    TH1F* fftResidual = RescaleAxis(hm,1./(end - start));
 
+    // TH1F* fftResidualInit = SetupFFT(residual, fitStartTime_, fitEndTime_);
+    //hm = fftResidualInit->FFT(hm,"MAG");
+    //int bins = input->GetNbinsX();
+     TAxis* xaxis = hm->GetXaxis();
+     double* ba = new double[n+1];
+     xaxis->GetLowEdge(ba);
+     double Scale = 1./(end - start);
+     ba[n] = ba[n-1] + xaxis->GetBinWidth(n);
+     for (int i = 0; i < n+1; i++) {
+       ba[i] *= Scale;
+     }
+     TH1F* fftResidual = new TH1F(hm->GetName(), hm->GetTitle(), n, ba);
+     for (int i = 0; i <= n; i++) {
+      fftResidual->SetBinContent(i, hm->GetBinContent(i));
+      fftResidual->SetBinError(i, hm->GetBinError(i));
+      }
+     // TH1F* fftResidual = RescaleAxis(hm,1./(end - start));
+    //TH1F* fftResidual = RescaleAxis(hm,1./(end - start));
+    fftResidual->SetTitle(";Frequency (MHz);Magnitude [Arb Units]");
+    fftResidual->SetStats(0);
+    fftResidual->SetName("residualFFT");
+    fftResidual->Scale(1.0 / fftResidual->Integral());
+    // hm->SetTitle(";Frequency (MHz);Magnitude [Arb Units]");
+    // hm->SetStats(0);
+    // hm->SetName("residualFFT");
+    // hm->Scale(1.0 / hm->Integral());
+
+    
     TCanvas *c1 = new TCanvas();
-    //    gpad->SetGrid;
-    hm->SetTitle("Magnitude of the 1st transform");
-    hm->Draw();
+    c1->SetGrid();
+    fftResidual->SetAxisRange(0,end/2,"X");
+    fftResidual->SetTitle("Magnitude of the 1st transform");
+    fftResidual->Draw();
     c1->SaveAs("figures/FFT_magtest.pdf");
+    // hm->SetTitle("Magnitude of the 1st transform");
+       // hm->Draw();
+    //c1->SaveAs("figures/FFT_magtest.pdf");
     //NOTE: for "real" frequencies you have to divide the x-axes range with the range of your function
     //(in this case 4*Pi); y-axes has to be rescaled by a factor of 1/SQRT(n) to be right: this is not done automatically!
   
--- IdealWiggle.cc
+++ IdealWiggle.cc
@@ -6,13 +6,12 @@
 #include "TH1F.h"
 #include "TH1D.h"
 #include "TCanvas.h"
+#include "TLegend.h"
 #include "TF1.h"
 #include "TRandom3.h"
 #include "TVirtualFFT.h"
-//#include "fftw3.h"
-using namespace std;
 
-//const Double_t Total_Time = 30.0 * 4200.0; // Global
+using namespace std;
 
 Double_t Wiggle(Double_t *x, Double_t *par){
   
@@ -21,13 +20,13 @@ Double_t Wiggle(Double_t *x, Double_t *par){
   double gamma     = 29.3;              // Magic gamma
   double A         = 0.05;              // Amplitude
   double omega     = 2 * TMath::Pi() / 4200;   // Time of a single wiggle ~ 4200 ns (omega=2pi/t)
-  double omega2     = 2 * TMath::Pi() / 10; // Random additional high frequency with T = 1000 ns
-  double omega3     = 2 * TMath::Pi() / 10000; // Random additional low frequency with T = 10000 ns
+  double omega2     = 2 * TMath::Pi() / 5000; // Random additional high frequency with T = 1000 ns
+  double omega3     = 2 * TMath::Pi() / 500; // Random additional high frequency with T = 10000 ns
   double phase     = TMath::Pi()/2;     //Phase angle 
   double N = par[0];                        
 
-  //  Double_t Npositrons = N * exp( - time / (tau * gamma)) * (1 + A * cos ( (omega * time) + phase)) ;
-   Double_t Npositrons =  N * exp(- time / (tau * gamma) ) * (1 + A * (cos ( (omega * time) + phase ) + cos ( (omega2 * time) + phase) + cos( (omega3 * time) + phase)));
+   Double_t Npositrons = N * exp( - time / (tau * gamma)) * (1 + A * cos ( (omega * time) + phase)) ;
+  // Double_t Npositrons =  N * exp(- time / (tau * gamma) ) * (1 + A * (cos ( (omega * time) + phase ) + cos ( (omega2 * time) + phase) + cos( (omega3 * time) + phase)));
  return Npositrons;
 }
 
@@ -47,27 +46,44 @@ int main() {
   //Instigate pseudorandom number generator 
   int seed = 12345; 
   TRandom3 *rand = new TRandom3(seed);
-
+ 
   // Loop over "events"
-  double numEvents = 2e+7;
-  for(int i=0; i<numEvents; i++) {
+  double numEvents = 1e+8;
+  for(int i=0; i<numEvents;i++) {
     double r1 = f1 -> GetRandom(0,totalTime);
-    // double log = TMath::Log(r1);
     h1 -> Fill(r1);
   }  
   
   double functionIntegral = f1->Integral(0,totalTime);
+  h1->Scale(1./functionIntegral);
   cout<<functionIntegral<<endl;
 
-
   //get FFT of residual
-
-  TH1 *fft = 0;
+  TH1 *hm =0;
   TVirtualFFT::SetTransform(0);
-  fft = h1 -> FFT(fft,"MAG");
-  //  fft -> SetStats(kFALSE); // ??
+  hm=h1->FFT(hm,"MAG"); 
 
+  //Rescale x-axis by dividing by the function domain 
+  TAxis* xaxis = hm->GetXaxis();
+  double* ba = new double[nBins+1];
+  xaxis->GetLowEdge(ba);
+  double Scale = 1./(totalTime - 0);
+  ba[nBins] = ba[nBins-1] + xaxis->GetBinWidth(nBins);
+  
+  for (int i = 0; i < nBins+1; i++) {
+       ba[i] *= Scale;
+  }
 
+  TH1F* fftResidual = new TH1F(hm->GetName(), hm->GetTitle(), nBins, ba);
+  for (int i = 0; i <= nBins; i++) {
+      fftResidual->SetBinContent(i, hm->GetBinContent(i));
+       fftResidual->SetBinError(i, hm->GetBinError(i));
+  }
+ 
+  fftResidual->SetTitle(";Frequency [Hz];Normalised Magnitude [Arb Units]");
+  fftResidual->SetStats(0);
+  fftResidual->SetName("residualFFT");
+  fftResidual->Scale(1.0 / fftResidual->Integral());
 
   TCanvas *c1 = new TCanvas();
   gPad->SetGrid();
@@ -78,24 +94,31 @@ int main() {
 
 
   TCanvas *c2 = new TCanvas();
-  gPad->SetGrid();
-  h1 -> SetTitle("Ideal Histogram;Time [ns];N");
+  // gPad->SetGrid();
+  h1 -> SetTitle(";Time [ns];Normalised Units");
+  h1 -> GetYaxis() -> SetMaxDigits(2);
   h1 -> Draw();
   //  c2 -> SetLogy();
   c2 -> SaveAs("figures/IdealHistogram.pdf");
 
   TCanvas *c3 = new TCanvas(); 
-  gPad->SetGrid();
-  fft -> SetTitle("FFT;Frequency [Hz] ?;Magnitude ?");
-  //  fft -> SetAxisRange(0,1/totalTime,"X");		
-  //  fft -> SetAxisRange(0,1/sqrt(numEvents),"Y");
+  //gPad->SetGrid();
+  //fftResidual -> SetTitle("FFT, Injected: #mbox{2#times10^{-3}} GHz & #mbox{2#times10^{-4}} GHz, g-2 freq: #mbox{2.38#times10^{-4}} GHz;Frequency [GHz];Magnitude [norm. units]");
+  fftResidual->SetTitle("FFT, g-2 freq: #mbox{2.38#times10^{-4}} GHz;Frequency [GHz];Magitude [norm. units]"); 
+  fftResidual -> SetAxisRange(0,0.004,"X");		
   c3 -> SetLogy();
-  fft -> Draw();
+  fftResidual->GetXaxis()->SetMaxDigits(2);
+  fftResidual -> SetMarkerColor(kRed+2);
+  fftResidual -> SetLineColor(kRed+2);
+  fftResidual -> Draw("HIST L SAME");
   c3 -> SaveAs("figures/FFT.pdf");
 
   delete f1;
   delete h1;
   delete c1;
   delete c2;
+  delete c3;
+  delete hm;
+  delete fftResidual;
   return 0;  
 }
